23a74edd3ccc6cacced6f9d6eaa40fd85dda5efc
dongalib1946 <dongalibrary@gmail.com>
2026-08-24 11:41:51 +0900

    Configure Apps Script backend URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.ATTENDANCE_CONFIG = {
-  appsScriptUrl: ""
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbxQyv9ARqZO0ybEVtxiE3V1Okv0A6_7PTbTHEHOPljI3rZE0nBvZ40mcRei-t0UhHtDcQ/exec"
 };
