4804ceaf8c1757a6ab2cc5225f612f4b1dcbec6d
dongalib1946 <dongalibrary@gmail.com>
2026-08-24 11:52:38 +0900

    Increase display system label size

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ATTENDANCE_CONFIG = {
-  version: "20260824-cache-bust-1",
+  version: "20260824-display-title-1",
   appsScriptUrl: "https://script.google.com/macros/s/AKfycbzuHEyyV84-8f2h-IOwYOpZA58_IvzBqwjwWIEo72_2uVmtJegY-ZSxqAVKAzg7H-SS1w/exec"
 };
