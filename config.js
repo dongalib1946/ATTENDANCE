c5fce624f8dba90138aefdbf49cd6f5e16bc57b2
dongalibrary-droid <dongalibrary@gmail.com>
2026-09-01 09:57:36 +0900

    Point display to updated Apps Script

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ATTENDANCE_CONFIG = {
-  version: "20260901-appscript-url-2",
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbxopg496FFI-I9KQXMyQVqL5Cjf__mKPhbAYI0O1xOrBTgONFH75BJoSCrEkMIEXvgdlg/exec"
+  version: "20260901-appscript-url-3",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbySwueULerPLgMtpRi4Oh5SYNSqLJv0N0Cs0ZdFkt0gNe_U-WpBrcmWZ0RptUww2_7R1A/exec"
 };
