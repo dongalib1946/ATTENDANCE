f4e9579a4d2c1dbf436abe90734e02d5d30a4650
dongalibrary-droid <dongalibrary@gmail.com>
2026-09-02 09:34:58 +0900

    Update Apps Script for recent logs

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ATTENDANCE_CONFIG = {
-  version: "20260902-recent-logs",
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbySwueULerPLgMtpRi4Oh5SYNSqLJv0N0Cs0ZdFkt0gNe_U-WpBrcmWZ0RptUww2_7R1A/exec"
+  version: "20260902-recent-logs-2",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzyBm7pCT55QwHyymvfCerO-b-5oFgNC2NdrpfZP9eNbGD9DAW3uqjzBeSdWXXDeEQN_A/exec"
 };
