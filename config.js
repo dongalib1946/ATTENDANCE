d9014b390c76af424d6c47f3c76842190308d834
Jay <Jay@Jay-MacBook.local>
2026-09-03 22:00:11 +0900

    Extend attendance cooldown to 40 seconds

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ATTENDANCE_CONFIG = {
-  version: "20260903-cache-bump-1",
+  version: "20260903-cache-bump-2",
   appsScriptUrl: "https://script.google.com/macros/s/AKfycbzyBm7pCT55QwHyymvfCerO-b-5oFgNC2NdrpfZP9eNbGD9DAW3uqjzBeSdWXXDeEQN_A/exec"
 };
