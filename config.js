bafaf53cf4de08ae78f4fd9bf42b259a2b9cb9ac
dongalibrary-droid <dongalibrary@gmail.com>
2026-09-16 15:39:40 +0900

    Add manual attendance modal

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ATTENDANCE_CONFIG = {
-  version: "20260904-dedupe-1",
+  version: "20260916-manual-1",
   appsScriptUrl: "https://script.google.com/macros/s/AKfycbxsiylbi04hHVCDaf06p4G0IIDYiNe6S9yBlR5QnBSAtCciC6pvptdtoeiF579ohI730g/exec"
 };
